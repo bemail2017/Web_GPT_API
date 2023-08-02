63284a76505bac3cba2bfe170cd92f7bf7fee088
bemail2017 <140424700+bemail2017@users.noreply.github.com>
2023-08-02 12:14:56 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+function sendMessage() {
+  const inputText = document.getElementById('input-text').value;
+  // Gửi yêu cầu tới API của OpenAI ở đây và xử lý câu trả lời
+  // Sau đó, hiển thị câu trả lời lên trang web
+}
