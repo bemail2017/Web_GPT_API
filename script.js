ede54a693181bd9514fa322c710406f1a968830e
bemail2017 <140424700+bemail2017@users.noreply.github.com>
2023-08-02 13:08:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // script.js
-const apiKey = "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"; // Đặt API Key của bạn ở đây
+const apiKey = "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";
 
 function sendMessage() {
   const inputText = document.getElementById('input-text').value;
@@ -17,7 +17,6 @@ function sendMessage() {
   })
   .then(response => response.json())
   .then(data => {
-    // Xử lý câu trả lời từ API của OpenAI ở đây
     displayAnswer(data.choices[0].text);
   })
   .catch(error => {
